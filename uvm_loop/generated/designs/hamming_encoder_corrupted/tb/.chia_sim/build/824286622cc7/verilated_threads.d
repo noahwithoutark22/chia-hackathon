4561c8b122815f0498e797809c0724962830f883
noah <yash.dubey.ece24@iitbhu.ac.in>
2026-09-25 22:07:14 +0530

    Replace hamming encoder generated design

--- uvm_loop/generated/designs/hamming_encoder_corrupted/tb/.chia_sim/build/824286622cc7/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /home/ray/anaconda3/share/verilator/include/verilated_threads.cpp \
- /home/ray/anaconda3/share/verilator/include/verilatedos.h \
- /home/ray/anaconda3/share/verilator/include/verilated_threads.h \
- /home/ray/anaconda3/share/verilator/include/verilated.h \
- /home/ray/anaconda3/share/verilator/include/verilated_config.h \
- /home/ray/anaconda3/share/verilator/include/verilated_types.h \
- /home/ray/anaconda3/share/verilator/include/verilated_funcs.h
